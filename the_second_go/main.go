1a86466cfb96af5779f3c26b1040f502b6d32112
IcoveJ <1443162545@qq.com>
2023-03-13 17:38:52 +0800

    Chore: image has done, pod is doing, GPUUsage need to do

--- the_second_go/main.go
+++ the_second_go/main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
+
+	"the_second_go/tools"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -20,6 +24,7 @@ import (
 
 const (
 	gpuMetricName = "nvidia.com/gpu"
+	srcfilepath = ""
 )
 
 type gpuUsage struct {
@@ -28,44 +33,103 @@ type gpuUsage struct {
 	GPUs          int64     `json:"gpus"`
 }
 
-func main() {
-	// 初始化Docker客户端
+// 初始化Docker客户端
+func initDocker() (*client.Client, error) {
 	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		log.Fatal(err)
+		glog.Error("Failed to init docker client, the error is %s", err)
+		return nil, err
 	}
+	
+	_, err = dockerClient.Ping(context.Background())
+	if err != nil {
+		glog.Error("Failed to connect to docker client, the error is %s", err)
+		return nil, err
+	}
+
+	glog.Info("Succeed to init docker client.")
+	return dockerClient, nil
+}
+
+// 初始化Kubernetes客户端
+func initK8S() (*kubernetes.clientSet, error){
+	// 加载kubeconfog
+	kubeConfig, errConfig := rest.InClusterConfig()
+	if errConfig != nil {
+		glog.Error("Failed to get kubeconfig, the error is %s", errConfig)
+		return nil, errConfig
+	}
+
+	// 创建k8s客户端
+	kubeClient, errClient := kubernetes.NewForConfig(kubeConfig)
+	if errClient != nil {
+		glog.Error("Failed to init kubeClient, the error is %s", errClient)
+		return nil, errClient
+	}
+
+	glog.Info("Succeed to init kubeClient")
+	return kubeClient, nil
+}
+
+// 制作镜像
+func createImage(filepath string) {
+	tools.ExecCommand("docker build -t ", filepath)
+}
+
+func main() {
+	// 初始化Docker客户端
+	dockerClient, err_docker := initDocker()
 	defer dockerClient.Close()
 
 	// 初始化Kubernetes客户端
-	kubeConfig, err := rest.InClusterConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
+	kubeClient, err_k8s := initK8S()
 
 	// 初始化Gin框架
 	router := gin.Default()
 
 	// 镜像打包和拉取
-	router.POST("/images", func(c *gin.Context) {
-		imageName := c.PostForm("image_name")
-		reader, err := dockerClient.ImagePull(context.Background(), imageName, types.ImagePullOptions{})
-		if err != nil {
-			log.Println(err)
-			c.AbortWithError(http.StatusInternalServerError, err)
+	router.POST("/image", func(c *gin.Context) {
+		// 用户指定的dockerfile保存路径
+		dstFilepath := c.PostForm("dst_path")
+		tools.CopyFile(srcfilepath, dstFilepath)
+
+		// 选择基础镜像：Ubuntu/CentOS
+		// os_version格式为: ubuntu:18.04
+		osVersion := c.PostForm("os_version")
+		cmd := "FROM " + osVersion + "\n"
+		tools.writeAtHead(dstFilepath, cmd)
+		
+		// 选择的python版本
+		pyVersion := c.PostFormArray("python_version")
+		tools.writeAtTail(dstFilepath, pyVersion)
+
+		// 选择镜像, 先拉取, 再将镜像名写入dockerfile中
+		imageArray := c.PostFormArray("image_name_choose")
+		imageNum := len(imageArray)
+		glog.Info("The num of image choosen by user is %s", imageNum)
+		for i := range imageArray {
+			reader, err := dockerClient.ImagePull(context.Background(), imageArray[i], types.ImagePullOptions{})
+			if err != nil {
+				glog.Error("Failed to pull docker image %s, the error is %s", imageArray[i], err)
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+			tools.WriteAtTail(dstFilepath, imageArray[i])
+			glog.Info("Succeed to pull docker image %s", imageArray[i])
+			defer reader.Close()
+		}
+
+		// 调用exec执行dockerfile，创建用户自定义镜像
+		imageName := c.PostForm("image_name_user")
+		cmd := "docker build -f "
+		err_exec := tools.ExecCommand(cmd, dstFilepath, " -t ", imageName)
+		if err_exec != nil {
+			glog.Error("Failed to exec docker build, the error is %s", err_exec)
 			return
 		}
-		defer reader.Close()
-		if _, err := client.CopyFromContainer(context.Background(), imageName, types.CopyToContainerOptions{}, "/"); err != nil {
-			log.Println(err)
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("Image %s has been pulled and packed.", imageName),
+			"message": fmt.Sprintf("Succeed to build image: %s", imageName),
 		})
 	})
 
@@ -73,21 +137,44 @@ func main() {
 	router.POST("/containers", func(c *gin.Context) {
 		containerName := c.PostForm("container_name")
 		memoryStr := c.PostForm("memory")
+		cpuStr := c.PostForm("cpu")
 		gpuStr := c.PostForm("gpu")
+		imageName := c.PostForm("image")
 
-		// 解析内存和GPU数
-		memory, err := resource.ParseQuantity(memoryStr)
-		if err != nil {
-			log.Println(err)
-			c.AbortWithError(http.StatusBadRequest, err)
+		// 解析内存、CPU和GPU数
+		memReq, err_mem := resource.ParseQuantity(memoryStr)
+		if err_mem != nil {
+			glog.Error("Failed to parse memReq, the error is %s", err_mem)
+			c.AbortWithError(http.StatusBadRequest, err_mem)
 			return
 		}
-		gpu, err := resource.ParseQuantity(gpuStr)
-		if err != nil {
-			log.Println(err)
-			c.AbortWithError(http.StatusBadRequest, err)
+		cpuReq, err_cpu := resource.ParseQuantity(cpuStr)
+		if err_cpu != nil {
+			glog.Error("Failed to parse cpu, the error is %s", err_cpu)
+			c.AbortWithError(http.StatusBadRequest, err_cpu)
 			return
 		}
+		gpuReq, err_gpu := resource.ParseQuantity(gpuStr)
+		if err_gpu != nil {
+			glog.Error("Failed to parse gpu, the error is %s", err_gpu)
+			c.AbortWithError(http.StatusBadRequest, err_gpu)
+			return
+		}
+
+		// 获取当前可用的Mem、CPU和PU
+		var avaGPU int64
+		m := make(map[int]uint64)
+		avaMem, avaCPU, m, _ := tools.GetAvailableMemoryAndGPU()
+		for i := range m {
+			avaGPU += m[i]
+		}
+
+		// 比较用户请求数和可用数
+		if memReq > avaMem | cpuReq > avaCPU | gpuReq > avaGPU {
+			glog.Error("Failed to alloc sources to create pod, because the free sources are limited!")
+			return
+		}
+
 
 		// 创建容器
 		pod := &v1.Pod{
---
+++ the_second_go/tools/tool.go
@@ -0,0 +1,142 @@
+package tools
+
+import (
+	"bufio"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/NVIDIA/gpu-monitoring-tools/bindings/go/nvml"
+	"github.com/golang/glog"
+	"github.com/shirou/gopsutil/mem"
+)
+
+func GetAvailableMemoryAndGPU() (uint64, int, map[int]uint64, error) {
+	// 获取系统可用内存
+	memInfo, _ := mem.VirtualMemory()
+	memAva := memInfo.Available / 1024 / 1024 / 1024
+
+	// 获取cpu核数
+	cpuCore := runtime.NumCPU()
+
+	// 获取GPU显存
+	err_init := nvml.Init()
+	if err_init != nil {
+		glog.Error("Failed to init nvml to get futher info, the error is %s", err_init)
+		return 0, 0, nil, err_init
+	}
+	defer nvml.Shutdown()
+
+	m := make(map[int]uint64)
+	// 获取当前机器上显卡数量，并获取每张显卡的数据
+	deviceCount, err_gpu := nvml.GetDeviceCount()
+	if err_gpu != nil {
+		glog.Error("Failed to get all GPU num, the error is %s", err_gpu)
+		return 0, 0, nil, err_gpu
+	}
+	for i := uint(0); i < deviceCount; i++ {
+		device, err_device := nvml.NewDeviceLite(uint(i))
+		if err_device != nil {
+			glog.Error("Failed to get GPU device, the error is %s", err_device)
+			return 0, 0, nil, err_device
+		}
+
+		deviceStatus, _ := device.Status()
+		usedMem := deviceStatus.Utilization.Memory
+		avaMem := *device.Memory - uint64(*usedMem)
+		m[0] = avaMem
+
+		glog.Info("GPU %s, the avaMem is %s", i, avaMem)
+	}
+
+	return memAva, cpuCore, m, nil
+}
+
+// 复制dockerfile，不破坏基础dockerfile
+func CopyFile(filepath string, newFilepath string) {
+	// 打开原始文件
+	src, err_src := os.Open(filepath)
+	if err_src != nil {
+		glog.Error("Failed to open original dockerfile, the error is %s", err_src)
+		return
+	}
+	defer src.Close()
+
+	// 创建目标文件
+	dst, err_dst := os.Create(newFilepath)
+	if err_dst != nil {
+		glog.Error("Failed to create target dockerfile, the error is %s", err_dst)
+		return
+	}
+	defer dst.Close()
+
+	// 复制文件内容
+	_, err_copy := io.Copy(dst, src)
+	if err_copy != nil {
+		glog.Error("Failed to copy file from src to target, the error is %s", err_copy)
+		return
+	}
+}
+
+// 从文件头追加写入数据
+func WriteAtHead(filepath string, content string) {
+
+	// 读取文件内容
+	data, err_read := ioutil.ReadFile(filepath)
+	if err_read != nil {
+		glog.Error("Failed to read dockerfile: %s", filepath)
+		return
+	}
+
+	// 打开文件并准备写入数据
+	file, err_open := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE, 0644)
+	if err_open != nil {
+		glog.Error("Failed to open dockerfile: %s", filepath)
+		return
+	}
+	defer file.Close()
+
+	// 将新数据追加到原始数据之前
+	_, err_write := file.WriteAt([]byte(content), 0)
+	if err_write != nil {
+		glog.Error("Failed to write content: %s", content)
+		return
+	}
+	_, err := file.WriteAt(data, int64(len(filepath)))
+	if err != nil {
+		glog.Error("Failed to write data: %s", data)
+		return
+	}
+
+}
+
+// 从文件尾追加写入数据
+func WriteAtTail(filepath string, imageArray []string) {
+	file, err := os.OpenFile(filepath, os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		glog.Error("Failed to open original dockerfile, the error is %s", err)
+		return
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for i := range imageArray {
+		s := "RUN pip install " + imageArray[i] + "\n"
+		writer.WriteString(s)
+	}
+}
+
+// 执行系统命令
+func ExecCommand(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		glog.Error("Failed to build new images, the error is %s", err)
+		return err
+	}
+	return nil
+}
+
+// 获取GPU信息
